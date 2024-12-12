30d113c73c311613eba02c323b4134b2f0a32354
Ulaş Sancak <ulas.sancak@hotmail.com.tr>
2024-12-12 16:07:40 +0300

    Use cell class instead of tags Add error case

---
+++ Sources/SVPinView/Classes/SVPinCell.swift
@@ -0,0 +1,15 @@
+//
+//  SVPinCell.swift
+//  SVPinView
+//
+//  Created by Ulaş Sancak on 12.12.2024.
+//
+
+import UIKit
+
+class SVPinCell: UICollectionViewCell {
+    @IBOutlet weak var pinField: SVPinField!
+    @IBOutlet weak var containverView: UIView!
+    @IBOutlet weak var placeholderLabel: UILabel!
+    @IBOutlet weak var underlineView: UIView!
+}
